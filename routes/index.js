8a22baf40e499cbbbd486964d5f5df1be318fba2
Jonty117k <47775113+Jonty117k@users.noreply.github.com>
2019-04-12 12:55:36 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,18 @@ request('https://newsapi.org/v1/articles?source=techcrunch&apiKey=d1043e950f034a
 });
 
 // Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/', function(req, res){
 	res.render('index');
 });
 
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
-}
+// function ensureAuthenticated(req, res, next){
+// 	if(req.isAuthenticated()){
+// 		return next();
+// 	} else {
+// 		//req.flash('error_msg','You are not logged in');
+// 		res.redirect('/users/login');
+// 	}
+// }
 
 module.exports = router;
